8d6e8b225ae95b6b1d8e306f52b3fbb0fe93987b
JoaoHenrique7 <joaohtristao@gmail.com>
2023-03-29 11:16:26 -0300

    Criando rota "FindById" e função "getById".

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -3,6 +3,7 @@ import { createUser } from '../useCases/user/CreateUser/CreateUser';
 import { getAllUsers } from "../useCases/user/GetAllUser/GetAllUsers";
 import { deleteUser } from '../useCases/user/DeleteUser/DeleteUser';
 import { findUserByCpfCnpj } from "../useCases/user/FindUserByCpfCnpj/FindUserByCpfCnpj";
+import { findById } from "../useCases/user/FindById/FindById";
 import { findUserByEmail } from "../useCases/user/FindUserByEmail/FindUserByEmail";
 import { findUserByUserName } from "../useCases/user/FindUserByUserName/FindUserByUserName";
 import { findUserByFullName } from "../useCases/user/FindUserByFullName/FindUserByFullName";
@@ -20,6 +21,10 @@ router.post('/createUser', (request: Request, response: Response) => {
 
 router.delete('/deleteUser', (request: Request, response: Response) => {
     return deleteUser.delete(request, response);
+});
+
+router.get('/findById', (request: Request, response: Response) => {
+    return findById.findById(request, response);
 });
 
 router.get('/findUserByEmail', (request: Request, response: Response) => {
