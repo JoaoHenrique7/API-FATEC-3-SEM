232dc163564ae1a55d2483a36bc103bce94fe390
YagoPSilva <yago.pereira1388@gmail.com>
2023-04-23 11:58:42 -0300

    feat: access control for users

--- frontend/src/pages/userManagement/listUserForm/ListUserForm.tsx
+++ frontend/src/pages/userManagement/listUserForm/ListUserForm.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./ListUserForm.module.css";
 import Navbar from "../../../modules/Navbar/Navbar";
 import Table from "../../../modules/Table/Table";
+import { Navigate } from "react-router-dom";
+import { Session } from "../../../model/utils/Session";
 
 class ListUserForm extends React.Component {
     redirectPage() {
@@ -10,21 +12,26 @@ class ListUserForm extends React.Component {
     }
 
     render() {
-      const breadcrumbList = [{name: "Home"}, {name: "Gerenciamento de Usuários"}];
-        return (
-            <div className={styles.content}>
-             <div className={styles.titleContainer}>
-                    <Navbar pathList={breadcrumbList}/>
-                    <h1>Listagem de Usuários</h1>
+        const breadcrumbList = [{ name: "Home" }, { name: "Gerenciamento de Usuários" }];
+        const session = Session();
+        if (session.profile.type === 1) {
+            return (
+                <div className={styles.content}>
+                    <div className={styles.titleContainer}>
+                        <Navbar pathList={breadcrumbList} />
+                        <h1>Listagem de Usuários</h1>
+                    </div>
+                    <div className={styles.container}>
+                        <button onClick={() => this.redirectPage()} className={styles.button} type="button">
+                            Adicionar Usuário
+                        </button>
+                        <Table />
+                    </div>
                 </div>
-                <div className={styles.container}>
-                    <button onClick={() => this.redirectPage()} className={styles.button} type="button">
-                    Adicionar Usuário
-                    </button>
-                    <Table />
-                </div>
-            </div>
-        );
+            );
+        } else {
+            return <Navigate to="/initialuser" />;
+        }
     }
 }
 
